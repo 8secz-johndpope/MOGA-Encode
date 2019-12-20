dfb17f7f4031be612ba88b020515f9e627ef2d9d
Oscar Andersson <osca.r.94.oa@gmail.com>
2019-12-20 13:56:58 +0100

    Minor changes

--- config.py
+++ config.py
@@ -11,16 +11,16 @@ LOG_PATH = "/output/logs/"
 CLI_VERBOSITY = "INFO"  # ERROR, WARNING, INFO, DEBUG
 
 # gen-alg parameters
-POP_SIZE = 4 * 4    # must be a multiple of 4 and larger than 5!
-NO_GENERATIONS = 30
+POP_SIZE = 4 * 3    # must be a multiple of 4 and larger than 5!
+NO_GENERATIONS = 6
 MOG_ALG = "nsga2"   # nsga2, nspso, moead
-EPOCHS = 3
+EPOCHS = 2
 PLOT_PATH = "/output/results/"
 FITNESS_DATA_PATH = "/output/results/"
 ML_PERFORMANCE_BASELINE = 0.76146 #TODO: update when using big model
 
 # optimization_problem parameters
-ML_DATA_INPUT = "/data/untouched_small/"  #TODO: use larger dataset
+ML_DATA_INPUT = "/data/untouched_big/"  #TODO: use larger dataset
 ML_DATA_OUTPUT = "/data/cityscapes/leftImg8bit/val/"
 VIDEO_ENCODER = "libx264" # "h264_nvenc" "libx264" "hevc_nvenc"
 JSON_PARAM_PATH = "encoding_parameters/" + VIDEO_ENCODER + "-parameters.json"
